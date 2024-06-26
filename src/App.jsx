c32b0e1589886d89d209abad3da9a02e6e884bef
Thomas Kruithof <tkruithof95@gmail.com>
2024-06-26 14:02:54 +0200

    solved lab

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Counter from "./components/Counter";
 import ClickablePicture from "./components/ClickablePicture";
 import Dice from "./components/Dice";
 import DiscoButton from "./components/DiscoButton";
+import Carousel from "./components/Carousel";
 
 function App() {
   return (
@@ -20,6 +21,14 @@ function App() {
         <DiscoButton />
         <DiscoButton />
       </div>
+      <Carousel
+        images={[
+          "https://randomuser.me/api/portraits/women/1.jpg",
+          "https://randomuser.me/api/portraits/men/1.jpg",
+          "https://randomuser.me/api/portraits/women/2.jpg",
+          "https://randomuser.me/api/portraits/men/2.jpg"
+        ]}
+      />
     </div>
   );
 }
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,21 +2,32 @@ import { useState } from "react"
 
 const Carousel = ({ images }) => {
 
-    const [currentImage, setCurrentImage] = useState(images[0]);
+    const [picturIndex, setPictureIndex] = useState(0)
+    const [currentImage, setCurrentImage] = useState(images[picturIndex]);
 
     const moveLeft = () => {
-
+        setPictureIndex((currentIndex) => (currentIndex === 0 ?
+            images.length - 1 : currentIndex - 1
+        ));
+        setCurrentImage(images[picturIndex])
     }
 
     const moveRight = () => {
-        
+        setPictureIndex((currentIndex) => (currentIndex === images.length - 1 ?
+            0 : currentIndex + 1
+        ));
+        setCurrentImage(images[picturIndex])
     }
+
+
 
     return (
         <div>
-            <button>Left</button>
+            <button onClick={moveLeft}>Left</button>
             <img src={currentImage} />
-            <button>Right</button>
+            <button onClick={moveRight}>Right</button>
         </div>
     )
 }
+
+export default Carousel;
